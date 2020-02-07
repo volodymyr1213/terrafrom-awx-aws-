e4ffbd714be5703784709e34e4ef6d40af485cea
Volodymyr Khomenko <45836705+volodymyr1213@users.noreply.github.com>
2020-02-06 23:00:03 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "s3_bucket" {}
+
+variable "s3_folder_project" {}
+ 
+variable "s3_bucket_region" {}
+
+variable "s3_folder_region" {}
+
+variable "s3_tfstate_file" {}
+
+variable "s3_folder_type" {}
+
+variable "private_subnet_id_1" {}
+
+variable "private_subnet_id_2" {}
+
+variable "private_subnet_id_3" {}
+
+variable "cluster_identifier" {}
+
+
+variable "s3_folder_name" {}
+
+variable "sec_group_name" {}
+
+variable "database_name" {}
+
+variable "region" {}
+
+
+variable "engine" {}
+variable "engine_version" {}
+
+variable "backup_retention_period" {}
+
+variable "port" {}
+
+variable "master_username" {}
+
+variable "master_password" {}
