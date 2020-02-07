6273ac34fb4e16e7164378ed612cd396a7c2cdef
Volodymyr Khomenko <45836705+volodymyr1213@users.noreply.github.com>
2020-02-06 22:58:30 -0500

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,26 @@
+  
+resource "aws_security_group" "allow_tls" {
+  name        = "${var.sec_group_name}"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
